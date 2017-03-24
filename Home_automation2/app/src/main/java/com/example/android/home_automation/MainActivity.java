974d4335d7b4a344f91505997b1c1cbb35d8c6cf
Aaarke <roshankumarroshi@gmail.com>
2017-03-24 15:50:15 +0530

    The project was developed By Aaarke(Roshan Kumar)
    Aaarke's Project

---
+++ Home_automation2/app/src/main/java/com/example/android/home_automation/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.home_automation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b2 = (Button)findViewById(R.id.button2);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("roshan") &&
+                        ed2.getText().toString().equals("aaarke@123")) {
+                    Intent i= new Intent(v.getContext(),Main2Activity.class);
+                    startActivity(i);
+
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
+
